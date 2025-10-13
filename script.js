d9b87228f86eae88f5189f623b91f4e10231a273
Mustafa yusuf <mustafyu@stud.ntnu.no>
2025-10-13 21:46:32 +0200

    first changes added

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Function to run when clicked add
+Function addTask() {
+
+    //Get the input element and read its value
+    const input = document.getElementById('taskInput');
+    const taskText = input.value.trim(); // trim() removes spaces before/after
+
+    //
+}
